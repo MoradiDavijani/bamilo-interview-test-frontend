4d8655f40ca2a5cc069f8b9062ea741d39f88992
Hossein Moradi Davijani <moradidavijani@gmail.com>
2017-10-11 00:01:38 +0330

    add table view for users page

--- src/scripts/admin-panel/users.controller.js
+++ src/scripts/admin-panel/users.controller.js
@@ -1,8 +1,46 @@
+import $ from 'jquery'
+import ServerSingleton from '../helpers/server'
+import * as sidebar from '../helpers/sidebar'
+
+const server = new ServerSingleton()
+
 const init = function() {
 	console.info('Admin Panel Users Controller Started')
-	let sidenav = $('nav.sidebar')
-	sidenav.find('.nav-link').removeClass('active')
-	sidenav.find('.nav-link[href="/admin-panel/users"]').addClass('active')
+	let page = new AdminUsersPage()
+	page.init()
+}
+
+class AdminUsersPage {
+	constructor () {
+		this.$table = $('.admin-users-page table tbody')
+		this.$reload = $('#reload')
+	}
+	
+	init () {
+		this.fetchUsers()
+		sidebar.changePage('users')
+		this.$reload.click(() => {
+			this.fetchUsers()
+		})
+	}
+	
+	fetchUsers () {
+		this.$table.addClass('loading')
+		server.fetch('/users', (users) => {
+			this.$table.html('')
+			users.forEach((user, index) => {
+				this.$table.append(
+					$('<tr>')
+						.append(
+							$('<td>').text(index + 1)
+						)
+						.append($('<td>').text(user.username)
+						)
+				)
+			})
+			this.$table.removeClass('loading')
+		})
+	}
 }
 
 exports.init = init
