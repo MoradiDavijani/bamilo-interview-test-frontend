d929052689b2882f778d426e97ea40813331d992
Hossein Moradi Davijani <moradidavijani@gmail.com>
2017-10-10 23:05:02 +0330

    add logout button

--- src/scripts/helpers/sidebar.js
+++ src/scripts/helpers/sidebar.js
@@ -2,13 +2,18 @@ import $ from 'jquery'
 import page from 'page'
 
 const init = () => {
-	$(document).on('click', '.sidebar-toggle', function() {
-		let $this = $(this),
-			$sidebar = $('.sidebar')
-		
-		$sidebar.toggleClass('active')
-		$this.toggleClass('active')
-	})
+	$(document)
+		.on('click', '.sidebar-toggle', function() {
+			let $this = $(this),
+				$sidebar = $('.sidebar')
+			
+			$sidebar.toggleClass('active')
+			$this.toggleClass('active')
+		})
+		.on('click', '#logout', function() {
+			localStorage.removeItem('token')
+			page.redirect('/login')
+		})
 }
 
 export { init }
